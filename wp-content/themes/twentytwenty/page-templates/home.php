0d1a058c213dca6cbd9cdd3997f41d2c2c691fd0
randomnessdev <randomnessdev@gmail.com>
2020-03-07 15:53:12 +0100

    Home

--- wp-content/themes/twentytwenty/page-templates/home.php
+++ wp-content/themes/twentytwenty/page-templates/home.php
@@ -6,9 +6,8 @@
 get_header();
 ?>
 
-
 <section class="main">
-        <video autoplay="true" muted="true">
+        <video autoplay="true" muted="true" loop>
             <source src="assets/img/bg.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
@@ -20,73 +19,101 @@ get_header();
                 <a href="#" class="btn btn-main">Book a walk</a>
             </div>
         </div>
-    </section>
+        <img class="bluepipe" src="/assets/img/bluepipe.svg" alt="Blue pipe">
+</section>
 
 
-    <section class="walk" id="section-walks">
+<section class="walk" id="section-walks">
+    <div class="section-intro">
         <h2>Themes</h2>
-        <p>Description of all themes</p>
+        <p>Our guided tours promote Brussels positively
+                at home and abroad by offering an authentic experience. Book a walk with us and discover
+                with us which female entrepreneurs are doing
+                the trick, how do microbreweries revive, what initiatives are making living more sustainable
+                and how is life here today in general.</p>
+    </div>
 
-        <article>
-            <img src="theme.png" alt="theme img" />
-            <div class="walk-desc">
-                <h3>Theme Name</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
-                    provident at ipsa</p>
-                <a href="#" class="btn">Book a walk</a>
-            </div>
-        </article>
-
-        <article>
-            <img src="theme.png" alt="theme img" />
-            <div class="walk-desc">
-                <h3>Theme Name</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
-                    provident at ipsa</p>
-                <a href="#" class="btn">Book a walk</a>
-            </div>
-        </article>
-
-        <article>
-            <img src="theme.png" alt="theme img" />
-            <div class="walk-desc">
-                <h3>Theme Name</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
-                    provident at ipsa</p>
-                <a href="#" class="btn">Book a walk</a>
-            </div>
-        </article>
-
-        <a class="btn">View more</a>
-    </section>
-
-    <section class="Tours" id="section-tours">
-        <h2>Tours</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam recusandae debitis et modi sed maxime eius
-            veritatis quam nisi vitae animi molestiae libero fugiat, architecto ut officiis ipsa ex. Vel.</p>
-
-        <div class="tour-types">
-            <article>
-                <img src="tour.png" alt="Tours">
-                <h2>Open tours</h2>
-            </article>
-            <article>
-                <img src="tour.png" alt="Tours">
-                <h2>Open tours</h2>
-            </article>
-            <article>
-                <img src="tour.png" alt="Tours">
-                <h2>Open tours</h2>
-            </article>
+    <article>
+        <div class="walk-img-container">
+            <img src="/assets/img/gp.webp" alt="theme img" />
         </div>
-    </section>
-    <section class="newsletter" id="section-newsletter">
-        <form action="#">
-            <label for="email">
-                <input type="email" name="email" id="email">
-            </label>
-        </form>
-    </section> 
+        <div class="walk-desc">
+            <h3>Theme Name</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
+                provident at ipsa</p>
+            <a href="#" class="btn">Book a walk</a>
+        </div>
+    </article>
+
+    <article>
+        <div class="walk-img-container">
+                <img src="/assets/img/gp.webp" alt="theme img" />
+        </div>
+        <div class="walk-desc">
+            <h3>Theme Name</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
+                provident at ipsa</p>
+            <a href="#" class="btn">Book a walk</a>
+        </div>
+    </article>
+
+    <article>
+            <div class="walk-img-container">
+                    <img src="/assets/img/gp.webp" alt="theme img" />
+            </div>
+            <div class="walk-desc">
+                <h3>Theme Name</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
+                    provident at ipsa</p>
+                <a href="#" class="btn">Book a walk</a>
+            </div>
+        </article>
+
+    <a href="#" class="btn btn-alt">View more</a>
+    <img src="assets/img/pinkpipeleft.svg" alt="Pink pipe" class="pink-pipe-left">
+</section>
+
+<section class="options" id="section-options">
+    <div class="section-intro">
+        <h2>Our options</h2>
+        <p>Are you on your own, coming with a group
+                of people or interested in team building or school
+                activity? For each walk, there are a lot of possibilities to choose from in our Bazaar
+                to go on the road with us.</p>
+        </div>
+    <div class="tour-types">
+        <article>
+            <h3>Open tours</h3>
+            <p>Type of tour descriptions</p>
+            <a href="#" class="btn">Choose a walk</a>
+        </article>
+        <article>
+            <h3>Open tours</h3>
+            <p>Type of tour descriptions</p>
+            <a href="#" class="btn">Choose a walk</a>
+        </article>
+        <article>
+            <h3>Open tours</h3>
+            <p>Type of tour descriptions</p>
+            <a href="#" class="btn">Choose a walk</a>
+        </article>
+        <article>
+            <h3>Open tours</h3>
+            <p>Type of tour descriptions</p>
+            <a href="#" class="btn">Choose a walk</a>
+        </article>
+        
+    </div>
+</section>
+<section class="newsletter" id="section-newsletter">
+    <p>Don’t miss out on any news. Subscribe for the monthly newsletter!</p>
+    <form action="#">
+        <label for="email">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+        </label>
+        <button type="submit" class="btn">Register to the newsletter</button>
+    </form>
+</section>
 
 <?php
 get_footer();
