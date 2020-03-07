36d3c43411cdd2244b87ce91936229be0c205244
zavalyshyn <zavalyshyn@gmail.com>
2020-03-07 16:30:36 +0100

    new home

--- wp-content/themes/twentytwenty/page-templates/home.php
+++ wp-content/themes/twentytwenty/page-templates/home.php
@@ -8,7 +8,7 @@ get_header();
 
 <section class="main">
         <video autoplay="true" muted="true" loop>
-            <source src="assets/img/bg.mp4" type="video/mp4">
+            <source src="wp-content/themes/twentytwenty/assets/images/bg.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
         <div class="copy">
@@ -19,7 +19,7 @@ get_header();
                 <a href="#" class="btn btn-main">Book a walk</a>
             </div>
         </div>
-        <img class="bluepipe" src="/assets/img/bluepipe.svg" alt="Blue pipe">
+        <img class="bluepipe" src="wp-content/themes/twentytwenty/assets/images/bluepipe.svg" alt="Blue pipe">
 </section>
 
 
@@ -32,22 +32,40 @@ get_header();
                 the trick, how do microbreweries revive, what initiatives are making living more sustainable
                 and how is life here today in general.</p>
     </div>
-
+<<?php
+    $args_sf = array(
+        'post_type' => 'walk',
+        'posts_per_page' => -1,
+        'suppress_filters' => 0
+    );
+    $loop = new wp_Query($args_sf);
+    while($loop->have_posts()) : $loop->the_post(); ?>
+    <!-- <a href="<?= get_permalink(); ?>" class="grid-item block block-simple block-did-u-know">
+        <div class="img-block">
+            <?= get_the_post_thumbnail($post->ID, 'category-thumb'); ?>
+        </div>
+        <div class="link-card"><?= the_title(); ?></div>
+    </a> -->
     <article>
         <div class="walk-img-container">
-            <img src="/assets/img/gp.webp" alt="theme img" />
+            <img src="<?= get_the_post_thumbnail_url($post->ID) ?>" alt="theme img" />
         </div>
         <div class="walk-desc">
-            <h3>Theme Name</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perferendis repellendus laborum
-                provident at ipsa</p>
-            <a href="#" class="btn">Book a walk</a>
+            <h3><?= the_title(); ?></h3>
+            <p><?= get_field('walk_description')?>)</p>
+            <a href="<?= get_permalink(); ?>" class="btn">Book a walk</a>
         </div>
     </article>
 
+    <?php
+    endwhile;
+    wp_reset_query();
+ ?>
+
+
     <article>
         <div class="walk-img-container">
-                <img src="/assets/img/gp.webp" alt="theme img" />
+                <img src="wp-content/themes/twentytwenty/assets/images/gp.webp" alt="theme img" />
         </div>
         <div class="walk-desc">
             <h3>Theme Name</h3>
@@ -59,7 +77,7 @@ get_header();
 
     <article>
             <div class="walk-img-container">
-                    <img src="/assets/img/gp.webp" alt="theme img" />
+                    <img src="wp-content/themes/twentytwenty/assets/images/gp.webp" alt="theme img" />
             </div>
             <div class="walk-desc">
                 <h3>Theme Name</h3>
@@ -70,7 +88,7 @@ get_header();
         </article>
 
     <a href="#" class="btn btn-alt">View more</a>
-    <img src="assets/img/pinkpipeleft.svg" alt="Pink pipe" class="pink-pipe-left">
+    <img src="wp-content/themes/twentytwenty/assets/images/pinkpipeleft.svg" alt="Pink pipe" class="pink-pipe-left">
 </section>
 
 <section class="options" id="section-options">
@@ -102,7 +120,7 @@ get_header();
             <p>Type of tour descriptions</p>
             <a href="#" class="btn">Choose a walk</a>
         </article>
-        
+
     </div>
 </section>
 <section class="newsletter" id="section-newsletter">
